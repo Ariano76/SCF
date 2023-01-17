71c16b63500628eff7fd8f7c355c3c36cf74a85c
Percy <percyher@hotmail.com>
2023-01-17 15:16:50 -0500

    delete file innecesarios

--- x_reportes/controlador_grafico_sin_parametro.php
+++
@@ -1,9 +0,0 @@
-<?php
-	require_once ('../administrador/config/bdPDO.php');
-	$situacion = $_POST['dato_situacion'];	
-	$sp = $_POST['dato_sp'];
-	$MG = new TransactionSCI();
-	$consulta = $MG -> traer_datos_reporte_sin_parametro($sp, $situacion);
-	echo json_encode($consulta);
-
-?>
